a6f0af2db306f4b79d37ce7b55fd040f48b07c64
thomasrdmd <thomasrdmd@gmail.com>
2024-01-25 12:14:42 +0100

    Added enemies and spawner
    Added enemies and enemy spawner.
    Enemy prefab and spawner search for a entity with the enemyTarget tag. The player character prefab needs this tag.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+    private GameObject target;
+    public float speed;
+
+    public int maxHealth = 5;
+    private int currentHealth;
+    public int damage;
+
+    private float distance;
+
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("EnemyTarget");
+        currentHealth = maxHealth;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        rigidbody2d.MovePosition(Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime));
+    }
+
+    void OnCollisionStay2D(Collision2D other){
+        PlayerMovementTemp playerScript = other.gameObject.GetComponent<PlayerMovementTemp>();
+
+        if(playerScript != null){
+            playerScript.changeHealth(-damage);
+        }
+    }
+
+    void changeHealth(int amount){
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log("Enemy health:" + currentHealth + "/" + maxHealth);
+        if(currentHealth <= 0){
+             Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy1Prefab;
+    [SerializeField]
+    private float enemyInterval = 3.5f;
+
+    private GameObject target;
+
+    public float minDistanceToTarget;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("EnemyTarget");
+        StartCoroutine(spawnEnemy(enemyInterval, enemy1Prefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy){
+        Vector2 spawnPosition = new Vector2(Random.Range(-10F, 10), Random.Range(-5f, 5f));
+        
+        if((target.transform.position.x - spawnPosition.x > minDistanceToTarget || target.transform.position.x - spawnPosition.x < -minDistanceToTarget)
+            &&
+            (target.transform.position.y - spawnPosition.y > minDistanceToTarget || target.transform.position.y - spawnPosition.y < -minDistanceToTarget) ) {
+             yield return new WaitForSeconds(interval);
+            GameObject newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+        
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
