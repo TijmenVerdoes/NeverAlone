7bceb4b7572b5362f65ac82c2e508869ca92937b
jckoolwijk <90912898+jckoolwijk@users.noreply.github.com>
2024-01-25 13:54:08 +0100

    lightMovement
    The light moves now in a pattern

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,6 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     public static float xPositionTorch = 0;
     public static float yPositionTorch = 0;
 
+    bool toTheRight = true;
+    bool toTheTop = true;
+
+    short lightHorizontalMovement = 5;
+    short lightVerticalMovement = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +27,47 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-       xPositionTorch = position.x;
-       yPositionTorch = position.y;
+
         //        Debug.Log(xPositionTorch);
         // Debug.Log(yPositionTorch); 
 
         
     //            Vector2 position = transform.position;
-    //    position.x = position.x + 0.1f;
-    //    position.y = position.y + 0.1f;
-    //    transform.position = position;
+if (toTheRight){
+    position.x = position.x + 0.0101f;
+        if (position.x > lightHorizontalMovement){
+            toTheRight = false;
+        }
+}
+else{
+        position.x = position.x - 0.0101f;
+        if (position.x < lightHorizontalMovement * -1){
+            toTheRight = true;
+        }
+}
+
+
+if (toTheTop){
+    position.y = position.y + 0.0151f;
+        if (position.y > lightVerticalMovement){
+            toTheTop = false;
+        }
+}
+else{
+        position.y = position.y - 0.00101f;
+        if (position.y < lightVerticalMovement * -1){
+            toTheTop = true;
+        }
+}
+
+       //position.x = position.x + 0.001f;
+       //position.y = position.y + 0.001f;
+
+              xPositionTorch = position.x;
+       yPositionTorch = position.y;
+
+
+       transform.position = position;
     }
 
 
