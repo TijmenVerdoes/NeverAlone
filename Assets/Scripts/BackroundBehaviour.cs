edf6d47a25153a264c14eb41c85e2cf37b88700d
Tijmen <Tijmenverdoes10@gmail.com>
2024-01-26 11:37:07 +0100

    Merge

--- Assets/Scripts/BackroundBehaviour.cs
+++ Assets/Scripts/BackroundBehaviour.cs
@@ -4,52 +4,45 @@ using UnityEngine;
 
 public class BackroundBehaviour : MonoBehaviour
 {
-
     float previousXPosition;
     float previousYPosition;
     bool firstRun = true;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (firstRun){
+        if (firstRun)
+        {
             firstRun = false;
-                    previousXPosition = NewBehaviourScript.xPositionTorch;
-        previousYPosition = NewBehaviourScript.yPositionTorch;
+            previousXPosition = NewBehaviourScript.xPositionTorch;
+            previousYPosition = NewBehaviourScript.yPositionTorch;
         }
 
-
-        float torchXPosition = NewBehaviourScript.xPositionTorch;
-        float xDifference = torchXPosition - previousXPosition;
+        var torchXPosition = NewBehaviourScript.xPositionTorch;
+        var xDifference = torchXPosition - previousXPosition;
         previousXPosition = torchXPosition;
-        float torchYPosition = NewBehaviourScript.yPositionTorch;
-        float yDifference = torchYPosition - previousYPosition;
+        var torchYPosition = NewBehaviourScript.yPositionTorch;
+        var yDifference = torchYPosition - previousYPosition;
         previousYPosition = torchYPosition;
 
         Vector2 position = transform.position;
-        position.x = position.x + xDifference;
-        position.y = position.y + yDifference;
+        position.x += xDifference;
+        position.y += yDifference;
         transform.position = position;
 
-       // sprite.transform.localScale = new Vector3(scale, scale, scale);
-       // Gets the local scale of game object
-Vector2 objectScale = transform.localScale;
+        // Gets the local scale of game object
+        Vector2 objectScale = transform.localScale;
 
 
-// Sets the local scale of game object
-transform.localScale = new Vector2(objectScale.x*0.99f,  objectScale.y*0.99f);
+        // Sets the local scale of game object
+        transform.localScale = new Vector2(objectScale.x * 0.99f, objectScale.y * 0.99f);
 
-    if (objectScale.x < 1){
-        transform.localScale = new Vector2(1,  1);
-    }
-     else{
- RubyController.maximumDistancePlayer = RubyController.maximumDistancePlayer*0.99f;
-     }
+        if (objectScale.x < 1)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            RubyController.maximumDistancePlayer *= 0.99f;
+        }
     }
 }
